b9c82d42bd5ec15478be05355a8ccb1feb55998a
Nasib Babazade <143509503+Nasib-cmd@users.noreply.github.com>
2024-12-06 17:18:49 +0400

    task 1 react component fix

--- React_component/task_0/dashboard/src/App/App.test.js
+++ React_component/task_0/dashboard/src/App/App.test.js
@@ -1,47 +1,32 @@
-import { shallow } from 'enzyme';
 import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
-import Login from '../Login/Login';
 
+describe('App Component', () => {
+	it('calls logOut prop when Control and h keys are pressed', () => {
+		const logOutMock = jest.fn();
+		const { getByText } = render(<App logOut={logOutMock} />);
 
-// shallow render app component
-describe('<App />', () => {
-	it('Tests that App renders without crashing', () => {
-		const wrapper = shallow(<App />);
-		expect(wrapper.exists()).toBe(true);
-	})
+		// Simulate pressing Control + h
+		fireEvent.keyDown(document, { ctrlKey: true, key: 'h' });
 
-	it('Contains Header component', () => {
-		const wrapper = shallow(<App />);
-		expect(wrapper.find('Header').length).toBe(1);
-	})
+		// Verify the logOut function was called
+		expect(logOutMock).toHaveBeenCalledTimes(1);
+	});
 
-	it('Contains Login component', () => {
-		const wrapper = shallow(<App />);
-		expect(wrapper.contains(<Login />)).toBe(true);
-	})
+	it('displays alert when Control and h keys are pressed', () => {
+		const logOutMock = jest.fn();
+		const alertMock = jest.spyOn(window, 'alert').mockImplementation(() => { });
 
-	it('Contains Footer component', () => {
-		const wrapper = shallow(<App />);
-		expect(wrapper.find('Footer').length).toBe(1);
-	})
+		render(<App logOut={logOutMock} />);
 
-	it('Tests that CourseList is not displayed', () => {
-		const wrapper = shallow(<App />);
-		expect(wrapper.find('CourseList').length).toBe(0);
-	})
+		// Simulate pressing Control + h
+		fireEvent.keyDown(document, { ctrlKey: true, key: 'h' });
+
+		// Verify the alert was called with the correct message
+		expect(alertMock).toHaveBeenCalledWith('Logging you out');
+
+		// Restore the original alert
+		alertMock.mockRestore();
+	});
 });
-
-
-// describe case when isLoggedIn is true
-describe('<App />', () => {
-	it('Tests that the Login component is not rendered', () => {
-		const wrapper = shallow(<App isLoggedIn={true} />);
-		expect(wrapper.contains(<Login />)).toBe(false);
-	})
-
-	it('Tests that CourseList component is rendered', () => {
-		const wrapper = shallow(<App isLoggedIn />);
-		expect(wrapper.find('CourseList').length).toBe(1);
-	})
-})
